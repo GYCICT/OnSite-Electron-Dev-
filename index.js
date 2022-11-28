b0780d9ab828923cfe99c1e83950df00e9f9c215
George Dellas <89891088+gorgdel@users.noreply.github.com>
2022-11-28 16:00:33 +1100

    adding license + new node module requirements

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow } = require('electron');
-
+const { autoUpdater } = require('electron-updater');
 // Send a message to the main process
+
+
+
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -27,3 +30,31 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
+
+autoUpdater.checkForUpdatesAndNotify();
+
+autoUpdater.on('update-available', () => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: 'A new version has been found. Restart the application to apply the updates.',
+  };
+});
+
+autoUpdater.on('update-downloaded', () => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: 'Update downloaded. Restart the application to apply the updates.',
+  };
+});
+
+autoUpdater.on('error', (message) => {
+  console.error('There was a problem updating the application');
+  console.error(message);
+});
+
+
+
