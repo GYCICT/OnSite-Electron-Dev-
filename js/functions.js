90d4952060a8397c9bec4f37c200231fb324e7e4
George Dellas <89891088+gorgdel@users.noreply.github.com>
2022-12-05 15:55:48 +1100

    Revert "Error checking for simon cookie expiry"
    This reverts commit abe46f56da69e194747479db247cfc8f58300ab0.

--- js/functions.js
+++ js/functions.js
@@ -115,12 +115,9 @@ function student_actions_show(id) {
 
             document.getElementById('spinner').hidden = true;
 
-        }, error: function (result) {
-            // Display the json error as an alerrt
-            let r = JSON.parse(result.responseJSON)
-            console.log(r)
-            createLog(r)
-            selector_error.setHTML('<p style="color: red;">' + r.Error + '</p>');
+        }, error: function (){
+            createLog('student_actions_show() failed')
+            selector_error.setHTML('An Error has occured - Student actions could not be loaded <br><br> Refresh the page and try again.')
         }
     });
 }
