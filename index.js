8259062f0dc3b61753e96beba29e94a9a6e9261b
wallen11 <103541377+wallen11@users.noreply.github.com>
2023-07-10 09:26:07 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
+// Send a message to the main process
+
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1520,
+    height: 900,
+    autoHideMenuBar: true,
+    fullscreen: true,
+  });
+
+  win.loadFile('index.html');
+
+  let updateVersion = `
+  let v = document.getElementById('version');
+  const version = 'v${app.getVersion()}';
+  v.innerHTML = version;
+  localStorage.setItem('version', version);
+  `;
+
+  win.webContents.executeJavaScript(updateVersion);
+
+};
+
+
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+autoUpdater.checkForUpdatesAndNotify();
+
+autoUpdater.on('update-available', () => {
+// show a dialog that lasts 3 seconds
+  setTimeout(() => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Update available',
+      message: 'A new update is available. It will be downloaded in the background',
+    });
+  }, 3000);
+});
+
+autoUpdater.on('update-downloaded', () => {
+    setImmediate(() => autoUpdater.quitAndInstall(isSilent = true, isForceRunAfter = true));
+});
+
+
+autoUpdater.on('error', (message) => {
+  console.error('There was a problem updating the application');
+  console.error(message);
+});
+
+setInterval(() => {
+  console.log('Checking for updates...');
+  autoUpdater.checkForUpdates();
+}
+, 60000);
+
+
+
+
